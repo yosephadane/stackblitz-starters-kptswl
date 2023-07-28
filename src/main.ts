6a36fc1c1d4fd28f81870f5fafe3aca8826148b8
yosephadane <44285100+yosephadane@users.noreply.github.com>
2023-07-28 10:15:58 +0300

    4 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,45 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app/app';
+import { StarRatingComponent } from './star-rating/star-rating';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
+@NgModule({
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatSortModule,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [AppComponent, StarRatingComponent],
+  bootstrap: [AppComponent],
+  providers: [],
 })
-export class App {
-  name = 'Angular';
-}
+export class App {}
 
 bootstrapApplication(App);
--- src/star-rating/star-rating.ts
+++ src/star-rating/star-rating.ts
@@ -1,42 +1,38 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
-import { MatSnackBar } from '@angular/material';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'star-rating',
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class StarRatingComponent implements OnInit {
+  @Input('rating') private rating: number = 3;
+  @Input('count') private count: number = 5;
 
-   @Input('rating') private rating: number = 3;
-   @Input('countRating') private count:number=5;
+  @Output() private ratingUpdated = new EventEmitter();
+  ratingArr: number[] = [];
 
-   @Output() private ratingUpdated = new EventEmitter();
-   ratingArr =[]
-   constructor(private matSnackBar:MatSnackBar){
-
-   }
-
-   ngOnInit(){
-
-    for(let i=0;i<this.count;i++)
-    {
-
+  ngOnInit() {
+    for (let i = 0; i < this.count; i++) {
       this.ratingArr.push(i);
     }
+  }
 
-   }
+  onClick(rating: number) {
+    this.ratingUpdated.emit(rating);
+    return false;
+  }
 
-   showIcon(index:number){
-    
-    if(this.rating<index+1)
-    return 'star'
-    else
-    return 'star_boarder'
-
-   }
-
-
-
+  showIcon(index: number) {
+    if (this.rating < index + 1) return 'star';
+    else return 'star_boarder';
+  }
 }
